d87c8c755dfbc3e4a2210bc13d99fef3815a9c77
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-11 12:44:20 +0000

    Wrapped elements in a `div` element

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ import styles from './Styles/Navigation.module.css';
 function App() {
   return (
     <>
-      <nav className={styles.navigation}>
-        <h1>Bookstore CMS</h1>
-        <ul className={styles.navlinks}>
-          <li>
-            <NavLink to="/" style={{ textDecoration: 'none', color: '#000' }}>Books</NavLink>
-          </li>
-          <li>
-            <NavLink to="categories" style={{ textDecoration: 'none', color: '#000', opacity: '0.5' }}>Categories</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<BooksPage />} />
-        <Route path="categories" element={<CategoriesPage />} />
-      </Routes>
+      <div className="container">
+        <nav className={styles.navigation}>
+          <h1>Bookstore CMS</h1>
+          <ul className={styles.navlinks}>
+            <li>
+              <NavLink to="/" style={{ textDecoration: 'none', color: '#000' }}>Books</NavLink>
+            </li>
+            <li>
+              <NavLink to="categories" style={{ textDecoration: 'none', color: '#000', opacity: '0.5' }}>Categories</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<BooksPage />} />
+          <Route path="categories" element={<CategoriesPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
